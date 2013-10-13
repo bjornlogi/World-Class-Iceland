81f5c98c18c97ee973aae670e0167f4c17f8297a
Karl Bachmann Stefánsson <kallibach@gmail.com>
2013-10-13 12:20:15 +0000

    Nýtt drawable, rounded_red_border

--- gen/hbv/wci/world_class_iceland/R.java
+++ gen/hbv/wci/world_class_iceland/R.java
@@ -30,6 +30,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
         public static final int red=0x7f020002;
         public static final int rounded_edittext=0x7f020003;
+        public static final int rounded_red_border=0x7f020004;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090012;

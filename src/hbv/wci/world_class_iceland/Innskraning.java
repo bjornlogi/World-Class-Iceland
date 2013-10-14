652b8a64175f69bc70d3da5570e840782041731c
Karl Bachmann Stefánsson <kallibach@gmail.com>
2013-10-14 13:27:22 +0000

    Javadoc komið fyrir Innskraning.java

--- src/hbv/wci/world_class_iceland/Innskraning.java
+++ src/hbv/wci/world_class_iceland/Innskraning.java
@@ -6,14 +6,32 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/** Activity sem sýnir innskráningar val og menu fyrir navigation.
+ * 
+ * @see         Activity
+ */
 public class Innskraning extends Activity {
 
+	/**
+	 * Býr til skjáinn, bindur layout úr activity_innskraning.xml við skjáinn
+	 *
+	 * @param savedInstanceState a Bundle which does something
+	 * @return none
+	 * @see Bundle
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_innskraning);
 	}
 
+	/**
+	 * Býr til valmynd fyrir skjáinn, hann kemur úr innskraning.xml
+	 * 
+	 * @param menu 
+	 * @return boolean gildi sem segir manni eitthvað
+	 * @see Menu
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -21,6 +39,13 @@ public class Innskraning extends Activity {
 		return true;
 	}
 	
+	/**
+	 * Stýrir á hvaða Activity verður kallað fyrir hvern af valmöguleikunum
+	 * 
+	 * @param item 
+	 * @return boolean gildi sem segir manni eitthvað
+	 * @see MenuItem
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {

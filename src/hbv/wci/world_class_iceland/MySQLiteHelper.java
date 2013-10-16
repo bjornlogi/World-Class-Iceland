c3d5c6f32f24684cde1931dafeff5f8945777a60
bjornlogi <bls4@hi.is>
2013-10-16 22:09:19 +0000

    async komid yfir i stundatoflu

--- src/hbv/wci/world_class_iceland/MySQLiteHelper.java
+++ src/hbv/wci/world_class_iceland/MySQLiteHelper.java
@@ -26,7 +26,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 	public static final String LOKAD = "lokad";
 	
 	private static final String DATABASE_NAME = "worldclass.db";
-	private static final int DATABASE_VERSION = 45;
+	private static final int DATABASE_VERSION = 46;
 	
 	public MySQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
--- src/hbv/wci/world_class_iceland/Stundatafla.java
+++ src/hbv/wci/world_class_iceland/Stundatafla.java
@@ -1,5 +1,7 @@
 package hbv.wci.world_class_iceland;
 
+import hbv.wci.world_class_iceland.TestDatabaseActivity.AsyncExecution;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,7 +9,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,7 +28,6 @@ import android.widget.Toast;
  * 
  */
 public class Stundatafla extends Activity{
-	  Timi [] timar = new Timi[10];
 	  private ListView mainListView ;
 	  private ArrayAdapter<String> listAdapter ;
 	  private DataSource mDataSource;
@@ -35,56 +42,123 @@ public class Stundatafla extends Activity{
 	    
 	    // Find the ListView resource. 
 	    mainListView = (ListView) findViewById( R.id.mainListView );
-
-	    // Create and populate a List of planet names.
-	    //List<? extends Map<String, String>> timar = new ArrayList<Map<String, String>>();
-	    
-	    List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-	    
-	    
-	    Timi testTimi = new Timi("Spinning", "8:00-9:00", "Salur 1", "Jón Jónsson");
-	    Timi testTimi2 = new Timi("Hot Yoga", "8:30-9:40", "Salur 3", "Jóna Jónsdóttir");
-	    timar[0] = testTimi;
-	    timar[1] = testTimi2;
 	    
 	    mDataSource = new DataSource(this);
 	    mDataSource.open();
 	    
-	    try{
-	    System.out.println(mDataSource.getAllHoptimar());
-	    }
-	    catch (Exception e){
-	    	System.out.println(e);
-	    }
+	    if (mDataSource.getAllHoptimarr().isEmpty())
+	    	new AsyncExecution().execute("http://www.worldclass.is/heilsuraekt/stundaskra");
+	    else
+	    	synaLista();
 	    
-	    list = mDataSource.getAllHoptimarr();
 	    
-//	    for (int i=0;i<2;i++){
-//	    	Map<String, String> map = new HashMap<String, String>(2);
-//	    	map.put("timi", timar[i].nafn);
-//	    	map.put("klukkan", timar[i].klukkan + "\n" + timar[i].salur);
-//	    	list.add(map);
-//	    }
-	    		//new String[] { "Spinning", "Zumba", "Hot Yoga", "Kross Fit",
-	              //                        "Buttlift", "Quickspinning", "Þol", "Jóga"};  
-	    
-	    SimpleAdapter adapter = new SimpleAdapter(this, list, android.R.layout.simple_list_item_2, new String[] {"timi", "klukkan", "salur", "tjalfari"}, new int[] {android.R.id.text1, android.R.id.text2}) ;
-	    
-//	    listAdapter.add( "Ceres" );
-	    
-	    // Set the ArrayAdapter as the ListView's adapter.
-	    mainListView.setAdapter( adapter );
-	    
-	    mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	  }// loka onCreate
+	  void synaLista(){
+		  	List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		    
+		    list = mDataSource.getAllHoptimarr();
+		    
+		    SimpleAdapter adapter = new SimpleAdapter(this, list, android.R.layout.simple_list_item_2, 
+		    		new String[] {"timi", "klukkan"}, 
+		    		new int[] {android.R.id.text1, android.R.id.text2}) ;
+		    
+		    // Set the ArrayAdapter as the ListView's adapter.
+		    mainListView.setAdapter( adapter );
+		    
+		    mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-	        @Override
-	        public void onItemClick(AdapterView<?> parnet, android.view.View view,
-	                int position, long id) {
-	        	//System.out.println(id);
-	        	String msg = "Svona muntu geta skráð þig í tima!";
-	        	Toast.makeText(Stundatafla.this, msg, 100000).show();
-	        }
-
-	    });
-	  }
-	}
+		        @Override
+		        public void onItemClick(AdapterView<?> parnet, android.view.View view,
+		                int position, long id) {
+		        	String msg = "Svona muntu geta skráð þig í tima!";
+		        	Toast.makeText(Stundatafla.this, msg, 100000).show();
+		        }
+		    });//loka onclick
+	  	}
+	  
+		public class AsyncExecution extends AsyncTask<String, Integer, String>{
+			
+			/**
+			 * 
+			 */
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+			}
+			
+			/**
+			 * @param params
+			 */
+			@Override
+			protected String doInBackground(String... params) {
+				String url=params[0];
+					 
+					try { 
+						Document doc = Jsoup.connect(url).get();
+						Elements tableElements = doc.select("table");
+						
+						Elements tableClassesElements = tableElements.select(":not(thead) tr");
+						String timar[] = {"", "morgun", "", "hadegi", "", "siddegi", "", "kvold"};
+						String dagar[] = {"Man", "Tri", "Mid", "Fim", "Fos", "Lau", "Sun"};
+						
+						for (int i = 0; i < tableClassesElements.size(); i++) {
+							Element row = tableClassesElements.get(i);
+							Elements rowItems = row.select("td");
+							String timi = timar[i];
+							
+							for (int j = 0; j < rowItems.size(); j++) {
+								String dagur = dagar[j];
+								Element list = rowItems.get(j);
+								Elements listItems = list.select("li"); 
+								
+								for (int k = 0; k < listItems.size(); k++){
+									String hopTimi[] = new String[9];
+									Element links = listItems.get(k);
+									hopTimi[0] = links.select("a").text();
+									hopTimi[1] = links.select(".stod").text();
+									hopTimi[2] = links.select(".salur").text();
+									hopTimi[3] = links.select(".tjalfari").text();
+									hopTimi[4] = links.select(".tegund").text();
+									hopTimi[5] = links.select(".time").text();
+									hopTimi[6] = timi;
+									hopTimi[7] = dagur;
+									Elements lokad = links.select(".locked");
+									
+									if (lokad.text() != "")
+										hopTimi[8] = lokad.attr("title");
+									else
+										hopTimi[8] = " ";
+									
+									mDataSource.addHoptimi(hopTimi);
+									
+								}
+							}
+						}
+						
+						System.out.println("til hamingju");
+					}
+					catch ( Exception e ) {
+						System.out.println("error: " + e);
+					}
+					
+					return "All Done!";
+			}
+			
+			/**
+			 * @param values
+			 */
+			@Override
+			protected void onProgressUpdate(Integer... values) {
+				super.onProgressUpdate(values);
+			}
+		
+			/**
+			 * @param result
+			 */
+			@Override
+			protected void onPostExecute(String result) {
+				synaLista();
+				super.onPostExecute(result);
+			}
+		}
+	}//loka Stundatafla

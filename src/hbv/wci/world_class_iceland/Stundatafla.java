23810c34eafbe5867b51d7529ab6e491ee32b186
Karl Bachmann Stefánsson <kallibach@gmail.com>
2013-10-17 09:23:24 +0000

    JavaDoc, vantar fyrir innbyggð föll.

--- src/hbv/wci/world_class_iceland/Stundatafla.java
+++ src/hbv/wci/world_class_iceland/Stundatafla.java
@@ -30,138 +30,156 @@ import android.widget.Toast;
  * @author Bjorn
  */
 public class Stundatafla extends Activity{
-	  private ListView mainListView ;
-	  private ArrayAdapter<String> listAdapter ;
-	  private DataSource mDataSource;
-	  
-	  /**
-	   * Called when the activity is first created.
-	   */
-	  @Override
-	  public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.stundatafla);
-	    
-	    // Find the ListView resource. 
-	    mainListView = (ListView) findViewById( R.id.mainListView );
-	    
-	    mDataSource = new DataSource(this);
-	    mDataSource.open();
-	    
-	    if (mDataSource.getAllHoptimarr().isEmpty())
-	    	new AsyncExecution().execute("http://www.worldclass.is/heilsuraekt/stundaskra");
-	    else
-	    	synaLista();
-	    
-	    
-	  }// loka onCreate
-	  void synaLista(){
-		  
-		  	List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-		    
-		    list = mDataSource.getAllHoptimarr();
-		    
-		    SimpleAdapter adapter = new SimpleAdapter(this, list, android.R.layout.simple_list_item_2, 
-		    		new String[] {"timi", "klukkan"}, 
-		    		new int[] {android.R.id.text1, android.R.id.text2}) ;
-		    
-		    // Set the ArrayAdapter as the ListView's adapter.
-		    mainListView.setAdapter( adapter );
-		    
-		    mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	private ListView mainListView ;
+	private ArrayAdapter<String> listAdapter ;
+	private DataSource mDataSource;
+	
+	/**
+	 * Called when the activity is first created.
+	 * Byr til skjainn og birtir upplysingar a honum.
+	 *
+	 * @param savedInstanceState
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.stundatafla);
+		
+		// Find the ListView resource. 
+		mainListView = (ListView) findViewById( R.id.mainListView );
+		
+		mDataSource = new DataSource(this);
+		mDataSource.open();
+		
+		if (mDataSource.getAllHoptimarr().isEmpty())
+			new AsyncExecution().execute("http://www.worldclass.is/heilsuraekt/stundaskra");
+		else
+			synaLista();
+		
+		
+	}// loka onCreate
+	
+	/**
+	 * Birtir gogn ur gagnagrunni a skja a listaformi.
+	 */
+	void synaLista(){
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		
+		list = mDataSource.getAllHoptimarr();
+		
+		SimpleAdapter adapter = new SimpleAdapter(this, list, android.R.layout.simple_list_item_2, 
+				new String[] {"timi", "klukkan"}, 
+				new int[] {android.R.id.text1, android.R.id.text2}) ;
+		
+		// Set the ArrayAdapter as the ListView's adapter.
+		mainListView.setAdapter( adapter );
+		
+		mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-		        @Override
-		        public void onItemClick(AdapterView<?> parnet, android.view.View view,
-		                int position, long id) {
-		        	String msg = "Svona muntu geta skráð þig í tima!";
-		        	Toast.makeText(Stundatafla.this, msg, 100000).show();
-		        }
-		    });//loka onclick
-	  	}
-	  
-		public class AsyncExecution extends AsyncTask<String, Integer, String>{
-			
-			/**
-			 * 
-			 */
 			@Override
-			protected void onPreExecute() {
-				super.onPreExecute();
+			public void onItemClick(AdapterView<?> parnet, android.view.View view,
+					int position, long id) {
+				String msg = "Svona muntu geta skráð þig í tima!";
+				Toast.makeText(Stundatafla.this, msg, 100000).show();
 			}
-			
-			/**
-			 * @param params
-			 */
-			@Override
-			protected String doInBackground(String... params) {
-				String url=params[0];
-					 
-					try { 
-						Document doc = Jsoup.connect(url).get();
-						Elements tableElements = doc.select("table");
+		});//loka onclick
+	}
+		
+	/**
+	 * Skrapar gogn af vef og setur inn i gagnagrunn ef hann er ekki til stadar.
+	 * 
+	 * @author Bjorn
+	 * @see AsyncTask
+	 */
+	public class AsyncExecution extends AsyncTask<String, Integer, String>{
+		
+		/**
+		 * 
+		 */
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+		
+		/**
+		 * Skrapar gogn af vef og setur inn i gagnagrunn i bakgrunns traedi.
+		 * 
+		 * @param params URL a sidunni sem sott er gogn af. 
+		 */
+		@Override
+		protected String doInBackground(String... params) {
+			String url=params[0];
+				 
+				try { 
+					Document doc = Jsoup.connect(url).get();
+					Elements tableElements = doc.select("table");
+					
+					Elements tableClassesElements = tableElements.select(":not(thead) tr");
+					String timar[] = {"", "morgun", "", "hadegi", "", "siddegi", "", "kvold"};
+					String dagar[] = {"Man", "Tri", "Mid", "Fim", "Fos", "Lau", "Sun"};
+					
+					for (int i = 0; i < tableClassesElements.size(); i++) {
+						Element row = tableClassesElements.get(i);
+						Elements rowItems = row.select("td");
+						String timi = timar[i];
 						
-						Elements tableClassesElements = tableElements.select(":not(thead) tr");
-						String timar[] = {"", "morgun", "", "hadegi", "", "siddegi", "", "kvold"};
-						String dagar[] = {"Man", "Tri", "Mid", "Fim", "Fos", "Lau", "Sun"};
-						
-						for (int i = 0; i < tableClassesElements.size(); i++) {
-							Element row = tableClassesElements.get(i);
-							Elements rowItems = row.select("td");
-							String timi = timar[i];
+						for (int j = 0; j < rowItems.size(); j++) {
+							String dagur = dagar[j];
+							Element list = rowItems.get(j);
+							Elements listItems = list.select("li"); 
 							
-							for (int j = 0; j < rowItems.size(); j++) {
-								String dagur = dagar[j];
-								Element list = rowItems.get(j);
-								Elements listItems = list.select("li"); 
+							for (int k = 0; k < listItems.size(); k++){
+								String hopTimi[] = new String[9];
+								Element links = listItems.get(k);
+								hopTimi[0] = links.select("a").text();
+								hopTimi[1] = links.select(".stod").text();
+								hopTimi[2] = links.select(".salur").text();
+								hopTimi[3] = links.select(".tjalfari").text();
+								hopTimi[4] = links.select(".tegund").text();
+								hopTimi[5] = links.select(".time").text();
+								hopTimi[6] = timi;
+								hopTimi[7] = dagur;
+								Elements lokad = links.select(".locked");
 								
-								for (int k = 0; k < listItems.size(); k++){
-									String hopTimi[] = new String[9];
-									Element links = listItems.get(k);
-									hopTimi[0] = links.select("a").text();
-									hopTimi[1] = links.select(".stod").text();
-									hopTimi[2] = links.select(".salur").text();
-									hopTimi[3] = links.select(".tjalfari").text();
-									hopTimi[4] = links.select(".tegund").text();
-									hopTimi[5] = links.select(".time").text();
-									hopTimi[6] = timi;
-									hopTimi[7] = dagur;
-									Elements lokad = links.select(".locked");
-									
-									if (lokad.text() != "")
-										hopTimi[8] = lokad.attr("title");
-									else
-										hopTimi[8] = " ";
-									
-									mDataSource.addHoptimi(hopTimi);
-									
-								}
+								if (lokad.text() != "")
+									hopTimi[8] = lokad.attr("title");
+								else
+									hopTimi[8] = " ";
+								
+								mDataSource.addHoptimi(hopTimi);
+								
 							}
 						}
-						
-						System.out.println("til hamingju");
-					}
-					catch ( Exception e ) {
-						System.out.println("error: " + e);
 					}
 					
-					return "All Done!";
-			}
-			
-			/**
-			 * @param values
-			 */
-			@Override
-			protected void onProgressUpdate(Integer... values) {
-				super.onProgressUpdate(values);
-			}
-		
-			/**
-			 * @param result
-			 */
-			@Override
-			protected void onPostExecute(String result) {
-				synaLista();
-				super.onPostExecute(result);
-			}
+					System.out.println("til hamingju");
+				}
+				catch ( Exception e ) {
+					System.out.println("error: " + e);
+				}
+				
+				return "All Done!";
 		}
-	}//loka Stundatafla
+			
+		/**
+		 * eitthvad
+		 * 
+		 * @param values
+		 */
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+	
+		/**
+		 * eitthvad
+		 * 
+		 * @param result
+		 */
+		@Override
+		protected void onPostExecute(String result) {
+			synaLista();
+			super.onPostExecute(result);
+		}
+	}
+}//loka Stundatafla

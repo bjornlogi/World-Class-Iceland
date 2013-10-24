a3edbd768deef493acb2f4770ee97f9dca687453
bjornlogi <bls4@hi.is>
2013-10-24 11:56:40 +0000

    Búinn að búa til test framework fyrir expandable list

---
+++ src/hbv/wci/world_class_iceland/Expandable.java
@@ -0,0 +1,12 @@
+package hbv.wci.world_class_iceland;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Expandable extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.expandable);
+	}
+
+}

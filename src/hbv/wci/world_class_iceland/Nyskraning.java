f596f2e1e06e3c13421d8bf1815e4fcc93962aa6
Sonja91 <sonjabara@gmail.com>
2013-10-31 11:22:48 +0000

    validation á nýskráningu

--- src/hbv/wci/world_class_iceland/Nyskraning.java
+++ src/hbv/wci/world_class_iceland/Nyskraning.java
@@ -10,6 +10,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Nyskraning extends Activity {
 	private DataSource mDataSource;
@@ -35,15 +39,54 @@ public class Nyskraning extends Activity {
 				mDataSource = new DataSource(mContext);
 				mDataSource.open();
 				
-				// TODO Validate the input to addUser, and toast if there is mismatch
-				//[netfang,lykilord,kennitala,stadfest,kort]
-				mDataSource.addUser(new String[]{netfang.getText().toString(),lykilord.getText().toString(),kennitala.getText().toString(),"nei","nei"});
 				
-				Intent j = new Intent(Nyskraning.this, Innskraning.class);
-				startActivity(j);
+				
+				// TODO Validate the input to addUser, and toast if there is mismatch
+				//[netfang,lykilord,kennitala,stadfest,kort],
+				// TODO ef gefid upp netfang, lykilord og ytt a nyskra, baeta vid gognum inn i nyskraningar inputin
+				//svo ekki turfi ad skrifa aftur
+				Boolean validEmail = validate(netfang.getText().toString());
+				Boolean noEmptyField = isNotEmptyNyskra(netfang.getText().toString(), lykilord.getText().toString(), kennitala.getText().toString(), lykilord2.getText().toString());
+				Boolean validKennitala = isKennitala(kennitala.getText().toString());
+				Boolean passwordMatch = passwordMatch(lykilord.getText().toString(), lykilord2.getText().toString());
+				if (validEmail && noEmptyField && validKennitala && passwordMatch)
+				{
+					mDataSource.addUser(new String[]{netfang.getText().toString(),lykilord.getText().toString(),kennitala.getText().toString(),"nei","nei"});
+					
+					Intent j = new Intent(Nyskraning.this, Innskraning.class);
+					startActivity(j);
+				}			
+				else{
+					Toast.makeText(Nyskraning.this, R.string.rangt, Toast.LENGTH_LONG).show();
+				}
+									
 			}
-		});
-		
+		});		
 	}
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+	public static boolean validate(String emailStr) {
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+	    return matcher.find();
+	}
+	public static boolean isNotEmptyNyskra(String email, String password, String kennitala, String lykilord2){
+		if(email != "" && password != "" && kennitala != "" && lykilord2 != "")
+			return true;
+		else
+			return false;		
+	}
+	public static boolean isKennitala(String kennitala){
+		if(kennitala.length() == 10)
+			return true;
+		else
+			return false;
+	}
+	public static boolean passwordMatch(String lykilord1, String lykilord2){
+		if(lykilord1.equals(lykilord2)){
+			return true;
+		}
+		else
+			return false;
+	}
+	 
 }
- 

7cab57272d0a9609813439918124c01ec0f372f3
bjornlogi <bls4@hi.is>
2013-10-31 08:54:01 +0000

    .

--- gen/hbv/wci/world_class_iceland/R.java
+++ gen/hbv/wci/world_class_iceland/R.java
@@ -8,9 +8,6 @@
 package hbv.wci.world_class_iceland;
 
 public final class R {
-    public static final class array {
-        public static final int country_arrays=0x7f070000;
-    }
     public static final class attr {
     }
     public static final class color {
@@ -30,99 +27,125 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int egilshollmynd=0x7f020001;
-        public static final int hafnarfjordurmynd=0x7f020002;
-        public static final int haskolinnireykjavikmynd=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int kopavogurmynd=0x7f020005;
-        public static final int kringlanmynd=0x7f020006;
-        public static final int laugarmynd=0x7f020007;
-        public static final int mosfellsbaermynd=0x7f020008;
-        public static final int ogurhvarfmynd=0x7f020009;
-        public static final int red=0x7f02000a;
-        public static final int rounded_edittext=0x7f02000b;
-        public static final int rounded_red_border=0x7f02000c;
-        public static final int rounded_submit_filter=0x7f02000d;
-        public static final int seltjarnarnesmynd=0x7f02000e;
-        public static final int sponginmynd=0x7f02000f;
+        public static final int drawer_shadow=0x7f020001;
+        public static final int egilshollmynd=0x7f020002;
+        public static final int hafnarfjordurmynd=0x7f020003;
+        public static final int haskolinnireykjavikmynd=0x7f020004;
+        public static final int ic_drawer=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int kopavogurmynd=0x7f020007;
+        public static final int kringlanmynd=0x7f020008;
+        public static final int laugarmynd=0x7f020009;
+        public static final int mosfellsbaermynd=0x7f02000a;
+        public static final int ogurhvarfmynd=0x7f02000b;
+        public static final int rounded_edittext=0x7f02000c;
+        public static final int rounded_red_border=0x7f02000d;
+        public static final int rounded_submit_filter=0x7f02000e;
+        public static final int seltjarnarnesmynd=0x7f02000f;
+        public static final int sponginmynd=0x7f020010;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f0a0012;
-        public static final int ScrollView01=0x7f0a0006;
-        public static final int btnSubmit=0x7f0a001d;
-        public static final int button1=0x7f0a0005;
-        public static final int content=0x7f0a001f;
-        public static final int dagur=0x7f0a0020;
-        public static final int dummy=0x7f0a0011;
-        public static final int editText1=0x7f0a0002;
-        public static final int editText2=0x7f0a0004;
-        public static final int egils_opnun=0x7f0a0007;
-        public static final int hfj_opnun=0x7f0a0008;
-        public static final int hr_opnun=0x7f0a0009;
-        public static final int imageView1=0x7f0a0000;
-        public static final int kop_opnun=0x7f0a000a;
-        public static final int kringlan_opnun=0x7f0a000b;
-        public static final int laugar_opnun=0x7f0a000c;
-        public static final int mainListView=0x7f0a001e;
-        public static final int mosfells_opnun=0x7f0a000d;
-        public static final int nes_opnun=0x7f0a000e;
-        public static final int ogur_opnun=0x7f0a0010;
-        public static final int opid=0x7f0a0015;
-        public static final int opnun_dagar1=0x7f0a0017;
-        public static final int opnun_header=0x7f0a0018;
-        public static final int opnun_menu=0x7f0a0021;
-        public static final int opnun_timar1=0x7f0a0016;
-        public static final int otimi_titill=0x7f0a0014;
-        public static final int pageviewtest_menu=0x7f0a0023;
-        public static final int skilabod=0x7f0a0019;
-        public static final int spinner1=0x7f0a001b;
-        public static final int spinner2=0x7f0a001c;
-        public static final int spong_opnun=0x7f0a000f;
-        public static final int stod_mynd=0x7f0a0013;
-        public static final int stundatafla_menu=0x7f0a0022;
-        public static final int textView1=0x7f0a0001;
-        public static final int textView2=0x7f0a0003;
-        public static final int viewpagermain=0x7f0a001a;
+        public static final int RelativeLayout1=0x7f090018;
+        public static final int ScrollView01=0x7f09000c;
+        public static final int dialogCancel=0x7f090028;
+        public static final int dialogSubmit=0x7f090027;
+        public static final int drawer_layout=0x7f090000;
+        public static final int dummy=0x7f090017;
+        public static final int egils_opnun=0x7f09000d;
+        public static final int expandable1=0x7f090029;
+        public static final int hfj_opnun=0x7f09000e;
+        public static final int hr_opnun=0x7f09000f;
+        public static final int imageView1=0x7f090001;
+        public static final int kennitalaInntak=0x7f090009;
+        public static final int kop_opnun=0x7f090010;
+        public static final int kringlan_opnun=0x7f090011;
+        public static final int laugar_opnun=0x7f090012;
+        public static final int lblListHeader=0x7f09002a;
+        public static final int left_drawer=0x7f090007;
+        public static final int lykilordInntakNr1=0x7f09000a;
+        public static final int lykilordInntakNr2=0x7f09000b;
+        public static final int lykilordInntakNr3=0x7f090004;
+        public static final int mosfells_opnun=0x7f090013;
+        public static final int nes_opnun=0x7f090014;
+        public static final int netfangInntak=0x7f090003;
+        public static final int netfangInntak2=0x7f090008;
+        public static final int nySkraLykilord=0x7f090026;
+        public static final int nySkraMerki=0x7f090024;
+        public static final int nySkraNetfang=0x7f090025;
+        public static final int nySkraTakki=0x7f090006;
+        public static final int ogur_opnun=0x7f090016;
+        public static final int opid=0x7f09001b;
+        public static final int opnun_dagar1=0x7f09001d;
+        public static final int opnun_header=0x7f09001e;
+        public static final int opnun_menu=0x7f09002d;
+        public static final int opnun_timar1=0x7f09001c;
+        public static final int otimi_titill=0x7f09001a;
+        public static final int scrollViewInnskraning=0x7f090002;
+        public static final int skilabod=0x7f09001f;
+        public static final int skraInnTakki=0x7f090005;
+        public static final int spinner1=0x7f090022;
+        public static final int spinner2=0x7f090023;
+        public static final int spong_opnun=0x7f090015;
+        public static final int stod_mynd=0x7f090019;
+        public static final int stundatafla_menu=0x7f09002e;
+        public static final int textView1=0x7f090021;
+        public static final int timiInfo=0x7f09002c;
+        public static final int timiTitill=0x7f09002b;
+        public static final int viewpagermain=0x7f090020;
     }
     public static final class layout {
         public static final int activity_innskraning=0x7f030000;
-        public static final int activity_opnunartimar=0x7f030001;
-        public static final int activity_opnunartimi=0x7f030002;
-        public static final int activity_pager=0x7f030003;
+        public static final int activity_nyskraning=0x7f030001;
+        public static final int activity_opnunartimar=0x7f030002;
+        public static final int activity_opnunartimi=0x7f030003;
         public static final int activity_stundatafla=0x7f030004;
-        public static final int pager=0x7f030005;
+        public static final int dialog_nyskra=0x7f030005;
+        public static final int drawer_list_item=0x7f030006;
+        public static final int expandable=0x7f030007;
+        public static final int list_group=0x7f030008;
+        public static final int list_item=0x7f030009;
     }
     public static final class menu {
-        public static final int menu_innskraning=0x7f090000;
+        public static final int menu_innskraning=0x7f080000;
+        public static final int menu_worldclass=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int bakgrunnur=0x7f060012;
-        public static final int country_prompt=0x7f060019;
-        public static final int dummy=0x7f06001a;
+        public static final int bakgrunnur=0x7f060014;
+        public static final int dummy=0x7f060023;
         public static final int egils=0x7f060008;
+        public static final int expandable=0x7f06001f;
         public static final int hello_world=0x7f060002;
         public static final int hfj=0x7f060006;
         public static final int hr=0x7f060009;
+        public static final int kennitala=0x7f060013;
         public static final int kpv=0x7f06000a;
         public static final int kringlan=0x7f060005;
         public static final int laugar=0x7f060004;
         public static final int lykilord=0x7f060010;
+        public static final int lykilordAftur=0x7f060011;
         public static final int mosfells=0x7f06000b;
         public static final int nes=0x7f06000c;
         public static final int netfang=0x7f06000f;
+        public static final int nySkra=0x7f060020;
         public static final int ogurkvarf=0x7f060007;
         public static final int opnunartimar=0x7f06000e;
-        public static final int pageview=0x7f060018;
-        public static final int skraInn=0x7f060011;
+        public static final int pageview=0x7f06001a;
+        public static final int rangt=0x7f060021;
+        public static final int skraInn=0x7f060012;
         public static final int sonsie=0x7f060003;
         public static final int spong=0x7f06000d;
-        public static final int stod_description=0x7f060013;
-        public static final int stodvar=0x7f060014;
-        public static final int stundatafla=0x7f060015;
-        public static final int testdb=0x7f060016;
-        public static final int weekday=0x7f060017;
+        public static final int stod_description=0x7f060015;
+        public static final int stodvar=0x7f060016;
+        public static final int stundatafla=0x7f060017;
+        public static final int testdb=0x7f060018;
+        public static final int velja=0x7f060022;
+        public static final int veljaStod=0x7f06001d;
+        public static final int veljaTegund=0x7f06001e;
+        public static final int velja_stod=0x7f06001b;
+        public static final int velja_tegund=0x7f06001c;
+        public static final int weekday=0x7f060019;
     }
     public static final class style {
         /** 
@@ -146,11 +169,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
-        public static final int CodeFont=0x7f080002;
+        public static final int AppTheme=0x7f070001;
+        public static final int CodeFont=0x7f070002;
     }
 }

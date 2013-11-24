081f5e5d27d4c48d73b51b276c243077064f10eb
bjornlogi <bls4@hi.is>
2013-11-24 14:38:43 +0000

    onclicklistener og sma fix

--- src/hbv/wci/world_class_iceland/stundatafla/StundataflanMin.java
+++ src/hbv/wci/world_class_iceland/stundatafla/StundataflanMin.java
@@ -49,7 +49,7 @@ public class StundataflanMin extends Activity {
 		setContentView(R.layout.activity_stundataflanmin);
 		
 		setDrawer();
-		
+		setOnButtonClickedListener();
 		data.open();
 		
 		StundatofluTimi st = data.getAllStundataflanMinTimi(this);
@@ -127,6 +127,17 @@ public class StundataflanMin extends Activity {
 			
 		});
 		
+	}
+	
+	private void setOnButtonClickedListener(){
+		final Button skraNyjanTima = (Button) findViewById(R.id.skraNyjanTima);
+		skraNyjanTima.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent j = new Intent(StundataflanMin.this, StundataflaNyrTimi.class);
+				startActivity(j);
+			}
+		});
 	}
 	
 	public void setDrawer()	{

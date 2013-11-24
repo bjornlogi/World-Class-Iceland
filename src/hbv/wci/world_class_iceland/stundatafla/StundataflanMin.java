9a487633f2d6bed5cc7943cee0dedc9ddc8a582b
Karl Bachmann Stefánsson <kallibach@gmail.com>
2013-11-24 11:46:15 +0000

    NavDrawer komið með stundataflan mín

--- src/hbv/wci/world_class_iceland/stundatafla/StundataflanMin.java
+++ src/hbv/wci/world_class_iceland/stundatafla/StundataflanMin.java
@@ -1,17 +1,40 @@
 package hbv.wci.world_class_iceland.stundatafla;
 
+import hbv.wci.world_class_iceland.Global;
 import hbv.wci.world_class_iceland.R;
 import hbv.wci.world_class_iceland.data.DataSource;
 import hbv.wci.world_class_iceland.data.StundatofluTimi;
+import hbv.wci.world_class_iceland.opnunartimar.Opnunartimar;
+import hbv.wci.world_class_iceland.skraning.Innskraning;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ListView;
 
 public class StundataflanMin extends Activity {
-
+	public Context mContext = this;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stundataflanmin);
+		
+		setDrawer();
 		
 		DataSource data = new DataSource(this);
 		data.open();
@@ -22,5 +45,130 @@ public class StundataflanMin extends Activity {
 		ExpandableListAdapter listAdapter;
 		listAdapter = new ExpandableListAdapter(this, st.listHeader, st.listChild, st.infoChild);
 		expListView.setAdapter(listAdapter);
+	}
+	
+	public void setDrawer()	{
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_stundataflan_min);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer_stundataflan_min);
+
+		// set a custom shadow that overlays the main content when the drawer opens
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		
+		// set up the drawer's list view with items and click listener
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, Global.drawerListItems));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		mDrawerToggle = new ActionBarDrawerToggle(
+			this,                  /* host Activity */
+			mDrawerLayout,         /* DrawerLayout object */
+			R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+			R.string.app_name,     /* "open drawer" description for accessibility */
+			R.string.app_name      /* "close drawer" description for accessibility */
+		){
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+	}
+	
+	/* The click listner for ListView in the navigation drawer */
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			mDrawerList.setItemChecked(position, true);
+			mDrawerLayout.closeDrawer(mDrawerList);
+			
+			String str = Global.drawerListItems[position];
+			if (str.equals(Global.ST1)) {
+				Intent i = new Intent(StundataflanMin.this, StundataflaActivity.class);
+				//i.putExtra("vikudagur", Integer.toString(Global.map.get(Global.dayOfWeek)));
+				startActivity(i);
+			} else if (str.equals(Global.ST2)){
+				
+				Intent i = new Intent(StundataflanMin.this, StundataflanMin.class);
+				//i.putExtra("vikudagur", Integer.toString(Global.map.get(Global.dayOfWeek)));
+				startActivity(i);
+				
+			} else if (str.equals(Global.OPN)){
+				Intent i = new Intent(StundataflanMin.this, Opnunartimar.class);
+				startActivity(i);
+			} else if (str.equals(Global.UTS)) {
+				Global.currentUser = null;
+				Global.currentUserID = -1;
+				SharedPreferences pref = mContext.getApplicationContext().getSharedPreferences("login", 0); // 0 - for private mode
+				Editor editor = pref.edit();
+				editor.clear();
+				editor.commit();
+				
+				Intent i = new Intent(StundataflanMin.this, Innskraning.class);
+				//i.putExtra("vikudagur", Integer.toString(Global.map.get(Global.dayOfWeek)));
+				startActivity(i);
+			} else if (str.equals(Global.INS)) {
+				Intent i = new Intent(StundataflanMin.this, Innskraning.class);
+				startActivity(i);
+			} else if (str.contains("@")) {
+				//Intent i = new Intent(StundataflanMin.this, UmNotenda.class);
+				//startActivity(i);
+			}
+		}
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggle
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+	
+	/* Called whenever we call invalidateOptionsMenu() */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content view
+		// check if user is logged in
+		if(Global.currentUser == null) {
+			Global.drawerListItems = new String[] {Global.ST1, Global.OPN, Global.INS};
+		} else {
+			Global.drawerListItems = new String[] {Global.currentUser, Global.ST1, Global.ST2, Global.OPN, Global.UTS};
+		}
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, Global.drawerListItems));
+		
+		//boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		//menu.findItem(R.id.opnun_menu).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
+	/**
+	 * Styrir i hvada Activity verdur kallad fyrir hvern af valmoguleikunum
+	 * 
+	 * @param item 
+	 * @return boolean gildi sem segir manni breytingin a Activity hafi gengid upp
+	 * @see MenuItem
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Drawer toggle button
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+	          return true;
+	    }
+		
+		return super.onOptionsItemSelected(item);
 	}
 }
